3bdb3c4f102c59a1f4ed5f8b9e590c4fd18f1943
SteveenDominguez <sdominguez@indoamericana.edu.co>
2023-07-31 15:54:06 -0500

    creando factorys para usuarios random

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(TipoDocumentoSeeder::class);
+
+        User::factory(5)->create(25);
     }
 }
